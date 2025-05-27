00ddb797fa7ecc7fcb9310fd7242b43ee94e9d77
Olga Klenova <olkaok-kanada@yandex.ru>
2025-05-28 00:26:42 +0500

    1 version

---
+++ data.py
@@ -0,0 +1,11 @@
+from random import randint
+
+class Person:
+    user_name = 'Ольга'
+    email = f'olga_klenova_22_777@yandex.ru'
+    password = f'Abc123456'
+
+class RandomData:
+    user_name = 'Тест'
+    email = f'test{randint(0, 999)}@yandex.ru'
+    password = f'{randint(1000, 9999)}Abc'
